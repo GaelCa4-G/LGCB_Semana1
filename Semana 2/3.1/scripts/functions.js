fb2a1c1e40b597bfc67b95708503cb4147e56356
GaelCa4-G <24308051220613@cbtis122.edu.mx>
2026-05-14 12:39:30 -0600

    Delete Semana 2 directory

--- Semana 2/3.1/scripts/functions.js
+++
@@ -1,9 +0,0 @@
-function votacion() {
-    let edad = parseInt(document.getElementById("Edad").value);
-    const resultado = document.getElementById("resultado");
-    if (edad >= 18) {
-        document.getElementById("resultado").textContent= "Eres apto para votar.";
-    } else {
-        document.getElementById("resultado").textContent= "No eres apto para votar.";
-    }
-}
